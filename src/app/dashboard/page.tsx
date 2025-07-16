e5e20324c32872cded3816af5d41d03f5ae402e4
Cayque Silva <cayque.ti@gmail.com>
2025-07-16 12:28:14 -0300

    feat: adição de redirecionamento de páginas a depender da s3ssao

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
-const DashboardPage = () => {
-    return ( 
-        <div>Dash</div>
-     );
-}
- 
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    return redirect("/authentication");
+  }
+  return <></>;
+};
+
 export default DashboardPage;
